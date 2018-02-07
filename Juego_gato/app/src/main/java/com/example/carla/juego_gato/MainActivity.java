afc835be680251e2db0ce17987d80489f7d6a847
Carlita <carla.cutte@hotmail.com>
2018-02-07 09:55:25 -0700

    codigo

--- Juego_gato/app/src/main/java/com/example/carla/juego_gato/MainActivity.java
+++ Juego_gato/app/src/main/java/com/example/carla/juego_gato/MainActivity.java
@@ -5,6 +5,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public class JuegoGato {
+        //Variables.
+        public char[][] tablero;
+        public char jugadorUno;
+        public char jugadorDos;
+        public boolean turno;
+        int contador;
+
+        //Constructor(Inicializa Variables).
+        public JuegoGato() {
+
+            tablero = new char[3][3];
+            jugadorUno = 'X';
+            jugadorDos = 'O';
+            turno = true;  //Si El Valor De "turno" Es Igual A "true" El Turno Es Del Jugador 1, De Lo Contrario Es Jugador 2.
+            int contador = 0;     //Lleva Cuenta De Las Veces Que Han Participado Los Jugadores 1 y 2.
+            inicializarTablero();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
